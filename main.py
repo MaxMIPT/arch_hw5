ae5111c5386a892235d02f8eac87377a510456ee
Max Pogrebnykh <maksim.pogrebnykh@glowbyteconsulting.com>
2025-04-13 15:09:56 +0300

    fix: memory reduced; speed increased; krasivo;

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 import requests
 
-def get_text(url):
-    response = requests.get(url)
-    return response.text
+from collections import Counter
 
-def count_word_frequencies(url, word):
-    text = get_text(url)
-    words = text.split()
-    count = 0
-    for w in words:
-        if w == word:
-            count += 1
-    return count
+
+words_file = "words.txt"
+url = "https://eng.mipt.ru/why-mipt/"
+
+def get_text_counter(url):
+    response = requests.get(url)
+    return Counter(
+        response.text.split()
+    )
 
 def main():
-    words_file = "words.txt"
-    url = "https://eng.mipt.ru/why-mipt/"
+    with open(words_file, "r") as f:
+        words = set(
+            f.read().split()
+        )
 
-    words_to_count = []
-    with open(words_file, 'r') as file:
-        for line in file:
-            word = line.strip()
-            if word:
-                words_to_count.append(word)
-
-    frequencies = {}
-    for word in words_to_count:
-        frequencies[word] = count_word_frequencies(url, word)
-    
+    original_text_counter = get_text_counter(url=url)
+    frequencies = {
+        w: original_text_counter.get(w, 0)
+        for w in words
+    }
     print(frequencies)
 
 if __name__ == "__main__":
